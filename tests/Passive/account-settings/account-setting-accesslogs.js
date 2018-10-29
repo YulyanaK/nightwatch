eb0e1c0ea8a518a8cfa781455d0fe8bce6fe39b8
Yulyana Karpava <yulyanakarpava@Yulyanas-MacBook-Pro.local>
2018-10-29 11:19:46 -0700

    account-settings update

--- tests/Passive/account-settings/account-setting-accesslogs.js
+++ tests/Passive/account-settings/account-setting-accesslogs.js
@@ -56,6 +56,19 @@ module.exports = {
   'Verify the organizations for profile notifications' : function(browser) {
       browser
         .useXpath()
+        .frame("intercom-borderless-frame")
+        .element('xpath', "//div[text()='Which one most closely matches your role?']", function(result){
+            if (result.value && result.value.ELEMENT) {
+                // Element is present, do the appropriate tests
+                browser.click("//div[text()='Which one most closely matches your role?']")
+                .waitForElementPresent("//div[@class='intercom-borderless-dismiss-button']", 6000)
+                .click("//div[@class='intercom-borderless-dismiss-button']");
+            } else {
+                // Element is not present.
+            }
+        });
+    browser
+        .frame(null)
         .waitForElementPresent("//div[text()='Ruwanda_QA']", 6000)
         //.verify.elementPresent('div.org-dashboard-card-container div:nth-of-type(3)', 'beta codes').pause(model.pause + 500)
         .click("//div[text()='Ruwanda_QA']")
