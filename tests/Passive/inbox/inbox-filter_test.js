4c8c2dfdc8d565a83845c3f55d6beed1f51c6322
Yulyana Karpava <yulyanakarpava@Yulyanas-MBP.attlocal.net>
2018-11-02 12:07:59 -0700

    inbox update

--- tests/Passive/inbox/inbox-filter_test.js
+++ tests/Passive/inbox/inbox-filter_test.js
@@ -8,17 +8,17 @@ module.exports = {
         //.url(model.url + '')
         .url('https://passive.glidr.io')
         .resizeWindow(1366, 768).pause(model.pause + 500)
-        .verify.elementPresent('div.login-logo.lpc-glidr-beta-login', 'looks for glidr logo').pause(model.pause + 500)
+        .waitForElementPresent('div.login-logo.lpc-glidr-beta-login', 6000, 'looks for glidr logo').pause(model.pause + 500)
         .verify.elementPresent('div.signin-form-container', 'seraches for active container for email').pause(model.pause + 500)
         .click('div.signin-form-container')
       browser
         .setValue('input[type=text]', 'dortiz+automation@glidr.io')
-        .verify.elementPresent('div.signin-form-container', 'searches for active container for password').pause(model.pause + 500)
+        .waitForElementPresent('div.signin-form-container', 6000, 'searches for active container for password').pause(model.pause + 500)
         .click('input[type=password]')
       browser
         .setValue('input[type=password]', 'Testtest1!')
         .click('div.signup-show-password')
-        .verify.elementPresent('div.login-button', 'checks for button is active').pause(model.pause + 500)
+        .waitForElementPresent('div.login-button', 6000, 'checks for button is active').pause(model.pause + 500)
         .click('div.login-button')
   },
 
